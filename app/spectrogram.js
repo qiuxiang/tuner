379d183a31783cfd7f8df6170d37fe510e844b3e
Qiu Xiang <xiang.qiu@qq.com>
2016-10-28 02:46:42 +0800

    optimize spectrogram performance

--- app/spectrogram.js
+++ app/spectrogram.js
@@ -2,15 +2,24 @@ var Spectrogram = function (selector) {
   this.canvas = document.querySelector(selector)
   this.canvas.width = document.body.clientWidth
   this.context = this.canvas.getContext('2d')
+  this.scale = 8
 }
 
 Spectrogram.prototype.update = function (data) {
-  var dataLength = parseInt(data.length * 0.8)
-  var barWidth = this.canvas.width / dataLength
+  var lengthScaled = parseInt(data.length * 0.75) / this.scale
+  var barWidth = this.canvas.width / lengthScaled
+  var barSum = 0
   this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
   data.reduce((function (x, value, index) {
-    this.context.fillStyle = 'hsl(' + (index * 360 / dataLength) + ', 100%, 50%)'
-    this.context.fillRect(x, this.canvas.height - value / 2, barWidth, value / 2)
-    return x + barWidth
+    barSum += value
+    if (index > 0 && (index % this.scale) == 0) {
+      value = barSum / this.scale
+      barSum = 0
+      this.context.fillStyle = 'hsl(' + (index / this.scale * 360 / lengthScaled) + ', 100%, 50%)'
+      this.context.fillRect(x, this.canvas.height - value / 2, barWidth, value / 2)
+      return x + barWidth
+    } else {
+      return x
+    }
   }).bind(this), 0)
 }
