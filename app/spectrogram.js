002bee88ecd6e9bd6d5679e23fa4fe381e69a5e6
Qiu Xiang <xiang.qiu@qq.com>
2016-10-31 17:09:02 +0800

    update spectrogram be simplified

--- app/spectrogram.js
+++ app/spectrogram.js
@@ -2,24 +2,15 @@ var Spectrogram = function (selector) {
   this.canvas = document.querySelector(selector)
   this.canvas.width = document.body.clientWidth
   this.context = this.canvas.getContext('2d')
-  this.scale = 2
 }
 
 Spectrogram.prototype.update = function (data) {
-  var lengthScaled = parseInt(data.length * 0.75) / this.scale
-  var barWidth = this.canvas.width / lengthScaled
-  var barSum = 0
+  var dataLength = parseInt(data.length * 0.125)
+  var barWidth = this.canvas.width / dataLength
   this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
   data.reduce((function (x, value, index) {
-    barSum += value
-    if (index > 0 && (index % this.scale) == 0) {
-      value = barSum / this.scale
-      barSum = 0
-      this.context.fillStyle = 'hsl(' + (index / this.scale * 360 / lengthScaled) + ', 100%, 50%)'
-      this.context.fillRect(x, this.canvas.height - value / 2, barWidth, value / 2)
-      return x + barWidth
-    } else {
-      return x
-    }
+    this.context.fillStyle = 'hsl(' + (index * 360 / dataLength) + ', 100%, 50%)'
+    this.context.fillRect(x, this.canvas.height - value / 2, barWidth, value / 2)
+    return x + barWidth
   }).bind(this), 0)
 }
