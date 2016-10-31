32229cad4bc3ab77817aceb13f25c7e19b0706fc
Qiu Xiang <xiang.qiu@qq.com>
2016-10-31 17:05:04 +0800

    add low-pass filter

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 var audioContext = new AudioContext()
 var analyser = audioContext.createAnalyser()
 analyser.fftSize = 4096
+var biquadFilter = audioContext.createBiquadFilter()
 var audioBuffer = new Float32Array(analyser.fftSize)
 var frequencyData = new Uint8Array(analyser.frequencyBinCount)
 var waveform = new Waveform('#waveform')
@@ -8,17 +9,12 @@ var spectrogram = new Spectrogram('#spectrogram')
 var pitchDetector = new (Module().PitchDetector)('default', analyser.fftSize, 1, audioContext.sampleRate)
 var $pitch = document.querySelector('#pitch')
 
-// var audio = new Audio()
-// var audioSource = audioContext.createMediaElementSource(audio)
-// audioSource.connect(analyser)
-// analyser.connect(audioContext.destination)
-// audio.controls = true
-// audio.crossOrigin = 'anonymous'
-// audio.src = 'http://mr3.doubanio.com/c255bf7d0a4195a129695298e2be18b3/0/fm/song/p576804_128k.mp3'
-// document.body.appendChild(audio)
+biquadFilter.type = 'lowpass'
+biquadFilter.frequency.value = 440
 
 navigator.mediaDevices.getUserMedia({audio: true}).then(function (stream) {
-  audioContext.createMediaStreamSource(stream).connect(analyser)
+  audioContext.createMediaStreamSource(stream).connect(biquadFilter)
+  biquadFilter.connect(analyser)
 })
 
 function noteFromFrequency(frequency) {
